7c8431329f0cf22ab56f4370dcf50d4d3b7537ba
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2018-12-12 20:43:47 -0600

    write in log.txt

--- liri.js
+++ liri.js
@@ -2,10 +2,13 @@
 require("dotenv").config();
 
 //Spotify package
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 
 //Code required to import the key.js
+//var keys = require("./keys.js");
+
 //var spotify = new Spotify(keys.spotify);
+
 
 //Bands in town node package
 //const bandsintown = require("bandsintown");
@@ -14,30 +17,47 @@ var Spotify = require('node-spotify-api');
 const axios = require("axios");
 
 //Moment package
-const moment = require('moment');
+const moment = require("moment");
 
-//var Events = new BandsInTownEvents;
+//fs package
+const fs = require("fs");
 
 //store the arguments entered by the user
 const request = process.argv[2];
 var search = process.argv.slice(3).join(" ");
 
+//Store the request made by the user 
+//This request will then be added to the log.txt file
+const textLog  = request + " " + search + ", ";
+
 //Depending on the user request we will execute the right function
 switch(request) {
     case "concert-this":
         concert();
+        addRequest();
         break;
     case "spotify-this-song":
         spotifyThisSong();
+        addRequest();
         break;
     case "movie-this":
         movieThis();
+        addRequest();
         break;
     case "do-what-it-says":
         doWIS();
+        addRequest();
         break;   
 }
 
+//function that write the request entered by the user in the log.txt file
+function addRequest() {
+    fs.appendFile("log.txt", textLog, function(err){
+        if (err) {
+            console.log(err);
+        } 
+    });
+};
 
 //If the concert function is called
 function concert() {
@@ -65,15 +85,43 @@ function concert() {
 
 //If the spotifyThisSong function is called
 function spotifyThisSong() {
-
+    spotify.search({ type: 'track', query: 'Without me' }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
 }
+
+
+// spotify
+//   .search({ type: 'track', query: 'All the Small Things' })
+//   .then(function(response) {
+//     console.log(response);
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+
+
+//   spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
 
 
 //If the movieThis function is called
 function movieThis() {
     //Test if the user typed a movie or not
-    if (process.argv[3] = " ") {
+    if (process.argv[3] = "") {
         let queryUrl = `http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy`;
+        console.log(queryUrl);
+
         axios
         .get(queryUrl)
         .then(function(response) {
@@ -105,3 +153,8 @@ function movieThis() {
         });      
     }
 };
+
+//If the doWIS function is called
+function doWIS() {
+
+}
