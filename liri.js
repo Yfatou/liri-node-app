f42b87916ebc5a6d0eaa6574267052c78f62f23c
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2018-12-13 18:34:34 -0600

    movie-this with undefined data cases

--- liri.js
+++ liri.js
@@ -5,13 +5,9 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 
 //Code required to import the key.js
-//var keys = require("./keys.js");
+var keys = require("./key");
 
-//var spotify = new Spotify(keys.spotify);
-
-
-//Bands in town node package
-//const bandsintown = require("bandsintown");
+var spotify = new Spotify(keys.spotify);
 
 //Axios package
 const axios = require("axios");
@@ -83,26 +79,22 @@ function concert() {
     });
 }
 
+
 //If the spotifyThisSong function is called
 function spotifyThisSong() {
-    spotify.search({ type: 'track', query: 'Without me' }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data); 
-      });
+
+    let queryUrl = `https://api.spotify.com/v1/search?q=${search}&type=track`;
+
+    console.log(queryUrl);
+
+    spotify
+    .request(queryUrl)
+    .then(function(response){
+        console.log(response);
+
+    });
 }
 
-
-// spotify
-//   .search({ type: 'track', query: 'All the Small Things' })
-//   .then(function(response) {
-//     console.log(response);
-//   })
-//   .catch(function(err) {
-//     console.log(err);
-//   });
 
 
 //   spotify
@@ -143,18 +135,39 @@ function movieThis() {
         .then(function(response) {
             console.log("\n***************");
             console.log(response.data.Title);
-            console.log("Came out in: " + response.data.Year);
-            console.log("Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-            console.log("Produced in: " + response.data.Country);
-            console.log("Langage: " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
+            checkIfExist("Came out in: " , response.data, "Year");
+            checkIfExist("Rating: " , response.data, "imdbRating");
+            // for (var i = 0; i < response.data.Ratings.length; i++) {
+            //     if (response.data.Ratings[i].Source == "Rotten Tomatoes") {
+            //         console.log("Rotten Tomatoes: " + response.data.Ratings[i].Value);
+            //     } else {
+            //         console.log ("No value for Rotten Tomatoes");
+            //     }
+            // }
+            checkIfExist("Rotten Tomatoes: " , response.data.Ratings[1], "Value");
+            checkIfExist("Produced in: " , response.data, "Country");
+            checkIfExist("Langage: " , response.data, "Language");
+            checkIfExist("Plot: " , response.data, "Plot");
+            checkIfExist("Actors: " + response.data.Actors);
         });      
     }
 };
 
 //If the doWIS function is called
 function doWIS() {
+
+}
+
+//Function to check if one of the data we want to display is unexisting
+//For exemple some movies doesn't have a "Rotten tomatoes" field or value
+//calling this checkIfExist function will allow us to print an error message
+//and finish printing the other existing datas
+function checkIfExist(text, field, value){
+
+    if (field === undefined){
+        console.log(text, "Nothing Found");
+    } else {
+        console.log(text, field[value]);
+    }
 
 }
