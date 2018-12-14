539f6b26588178e6578a7b31a25e8cce0f38a52e
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2018-12-14 12:06:54 -0600

    Spotify without no value entered by the user

--- liri.js
+++ liri.js
@@ -5,8 +5,9 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 
 //Code required to import the key.js
-var keys = require("./key");
+var keys = require("./key.js");
 
+//Access the keys information
 var spotify = new Spotify(keys.spotify);
 
 //Axios package
@@ -24,7 +25,7 @@ var search = process.argv.slice(3).join(" ");
 
 //Store the request made by the user 
 //This request will then be added to the log.txt file
-const textLog  = request + " " + search + ", ";
+const textLog  = request + " " + search + ", " + "\n";
 
 //Depending on the user request we will execute the right function
 switch(request) {
@@ -66,8 +67,10 @@ function concert() {
     axios
     .get(queryUrl)
     .then(function(response){
+        //if (!search){ console.log("There is no concert for: " + search); }
         console.log("\n***************");
-        for (var i = 0; i < response.data.length; i++) {
+       for (var i = 0; i < response.data.length; i++) {
+            console.log(response.data[i].lineup[0]);
             console.log("Venue: " + response.data[i].venue.name);
             console.log("Location: " + response.data[i].venue.country + ", " + response.data[i].venue.city);
             //Put the date to the right format
@@ -75,42 +78,49 @@ function concert() {
             console.log("Date: " + eventDate);
             console.log("\n***************");
         }
-       
     });
 }
 
 
 //If the spotifyThisSong function is called
 function spotifyThisSong() {
+    // if(process.argv[3] = "") {
+    //     search = "The Sign Ace of Base";
 
-    let queryUrl = `https://api.spotify.com/v1/search?q=${search}&type=track`;
+    //     spotify
+    //     .search ({ type: 'track', query: search, limit: 5}, function(err, data) {
+    //         if (err) {
+    //             console.log(err);
+    //         }
 
-    console.log(queryUrl);
+    //         //song data from the API
+    //         console.log("Artist: " + data.tracks.items[0].album.artists[0].name)
+    //         console.log("Song name: " + data.tracks.items[0].name);
+    //         console.log("Preview URL: " + data.tracks.items[0].album.external_urls.spotify);
+    //         console.log("Album: " + data.tracks.items[0].album.name);
+    //     });
+    // } else {
 
-    spotify
-    .request(queryUrl)
-    .then(function(response){
-        console.log(response);
-
-    });
-}
-
-
-
-//   spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
+        spotify
+        .search ({ type: 'track', query: search, limit: 5}, function(err, data) {
+            if (err) {
+                console.log(err);
+            }
+        console.log(search);
+        //song data from the API
+        console.log("Artist: " + data.tracks.items[0].album.artists[0].name)
+        console.log("Song name: " + data.tracks.items[0].name);
+        console.log("Preview URL: " + data.tracks.items[0].album.external_urls.spotify);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        });
+    }
+//}
 
 
 //If the movieThis function is called
 function movieThis() {
     //Test if the user typed a movie or not
-    if (process.argv[3] = "") {
+    if (process.argv[3] = "") {//If the user did not type a movie, we display the informations for Mr Nobody
         let queryUrl = `http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy`;
         console.log(queryUrl);
 
@@ -119,6 +129,8 @@ function movieThis() {
         .then(function(response) {
             console.log("\n***************");
             console.log(response.data.Title);
+            console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
             console.log("Came out in: " + response.data.Year);
             console.log("Rating: " + response.data.imdbRating);
             console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
@@ -137,13 +149,6 @@ function movieThis() {
             console.log(response.data.Title);
             checkIfExist("Came out in: " , response.data, "Year");
             checkIfExist("Rating: " , response.data, "imdbRating");
-            // for (var i = 0; i < response.data.Ratings.length; i++) {
-            //     if (response.data.Ratings[i].Source == "Rotten Tomatoes") {
-            //         console.log("Rotten Tomatoes: " + response.data.Ratings[i].Value);
-            //     } else {
-            //         console.log ("No value for Rotten Tomatoes");
-            //     }
-            // }
             checkIfExist("Rotten Tomatoes: " , response.data.Ratings[1], "Value");
             checkIfExist("Produced in: " , response.data, "Country");
             checkIfExist("Langage: " , response.data, "Language");
@@ -158,15 +163,16 @@ function doWIS() {
 
 }
 
-//Function to check if one of the data we want to display is unexisting
+//Function to check if one of the data we want to display is non-existent
 //For exemple some movies doesn't have a "Rotten tomatoes" field or value
 //calling this checkIfExist function will allow us to print an error message
 //and finish printing the other existing datas
 function checkIfExist(text, field, value){
-
+    //If the field we're looking for in the JSON file is non-existent 
     if (field === undefined){
         console.log(text, "Nothing Found");
     } else {
+        //if the field is there, we print it preceded by the text
         console.log(text, field[value]);
     }
 
